61451eb0c9dcb8d1bf092d2ad755c0e251ee36b5
Aisha Bello <2426303b@student.gla.ac.uk>
2020-02-11 13:13:00 +0000

    chapter 4 completed

--- rango/views.py
+++ rango/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
-def about(request):
-    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
-
 #def index(request):
-#    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
-#    return render(request, 'rango/index.html', context=context_dict)
+    #return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
+#def about(request):
+    #return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+
+def index(request):
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
+def about(request):
+    context_dict = {'boldmessage': 'This tutorial has been put together by Aisha Bello'}
+    return render(request, 'rango/about.html', context=context_dict)
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -19,9 +19,13 @@ from django.urls import include, path
 from rango import views
 from django.conf.urls import url
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('rango/', include('rango.urls')),
     path('admin/', admin.site.urls),
-]
+    
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
